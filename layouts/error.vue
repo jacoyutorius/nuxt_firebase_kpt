a55f33bbe645c857cd7ab093ec9c0d03ff243931
yutoogi <yuto.ogi@fourier.jp>
2018-04-16 22:05:16 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="section">
+    <div class="content">
+      <h1 v-if="error.statusCode === 404">ページが見つかりません</h1>
+      <h1 v-else>エラーが発生しました</h1>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: ['error']
+}
+</script>
